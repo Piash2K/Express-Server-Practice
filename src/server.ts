ed7fe2651d5eb8170cfe5b271f89c71713aee3c7
PIASH <zmnobab1999@gmail.com>
2025-12-02 21:51:04 +0600

    handle not found route

--- src/server.ts
+++ src/server.ts
@@ -155,6 +155,15 @@ app.delete("/students/:id", async (req: Request, res: Response) => {
   }
 });
 
+//handle not found route
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
